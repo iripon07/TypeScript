fe9a2895ba3cbcfa218cd373702f38dd14125948
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-01-08 01:01:06 +0600

    mapped

---
+++ src/module3/condition.ts
@@ -0,0 +1,14 @@
+// type a1 = number;
+// type a3 = undefined;
+// type a4 = number;
+
+// type a2 = a1 extends string ? string : null;
+// ////Nested conditional types
+
+// type d = a1 extends null
+//   ? null
+//   : a3 extends number
+//   ? number
+//   : a4 extends null
+//   ? null
+//   : never;
