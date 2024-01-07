524b25319a2eb3d9b5539261e07eb418229ee4ad
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-01-07 23:38:21 +0600

    generic constraint

---
+++ src/module3/generic-constraint.ts
@@ -0,0 +1,30 @@
+// type MandatoryTypes = { name: string; age: number; salary: number };
+// interface MandatoryInterface {
+//   name: string;
+//   age: number;
+//   salary: number;
+// }
+
+// const addMeInMyCrushMind = <T extends MandatoryInterface>(myInfo: T) => {
+//   const crush = "Kristen Stewart";
+//   const newData = { ...myInfo, crush };
+//   return newData;
+// };
+
+// type MyInfoType = {
+//   name: string;
+//   age: number;
+//   salary: number;
+//   other1: boolean;
+//   other2: null;
+// };
+// const myInfo = {
+//   name: "Villain",
+//   age: 100,
+//   salary: 100,
+//   other1: false,
+//   other2: null,
+// };
+
+// const result5 = addMeInMyCrushMind<MyInfoType>(myInfo);
+// result5;
---
+++ src/module3/generic-keyof-constraints.ts
@@ -0,0 +1,15 @@
+type PersonType ={
+  name:string
+  age:number
+  address:string
+}
+
+type newType = "name"|"age"|"address"
+type newTypeUsingKeyOf = keyof PersonType
+const a:newType="age"
+
+function getProperty<X, Y extends keyof X>(obj:X, key:Y){
+  obj[key]
+}
+
+const property = getProperty({name:"Mr.X", age:100}, "age")
