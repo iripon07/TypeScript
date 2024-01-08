060c6e6f60fc1fa59d1f6046b82c8ee6047c3ee2
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-01-08 10:37:45 +0600

    object and inheritance

---
+++ src/module4/object.ts
@@ -0,0 +1,22 @@
+// class Animal {
+//   // public name: string;
+//   // public species: string;
+//   // public sound: string;
+//   constructor(
+//     public name: string,
+//     public species: string,
+//     public sound: string
+//   ) {
+//     // this.name = name;
+//     // this.species = species;
+//     // this.sound = sound;
+//   }
+//   public makeSound() {
+//     console.log(`${this.name} says ${this.sound}`);
+//   }
+// }
+
+// const dog = new Animal("German Shepherd", "dog", "Ghew Ghew");
+// const cat = new Animal("Persian", "Cat", "Mew Mew");
+// dog.makeSound();
+// cat.makeSound();
