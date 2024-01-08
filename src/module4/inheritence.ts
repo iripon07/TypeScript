a8746bb010a96a47ed59ec47d3884ddf92c62051
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-01-08 13:16:53 +0600

    polymorphism

--- src/module4/inheritence.ts
+++ src/module4/inheritence.ts
@@ -1,31 +1,31 @@
-class Parent {
-  name: string;
-  age: number;
-  address: string;
-  constructor(name: string, age: number, address: string) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
-  }
-  makeSleep(hours: number): string {
-    return `This ${name} will sleep for ${hours} `;
-  }
-}
-class Student extends Parent {
-  constructor(name: string, age: number, address: string) {
-    super(name, age, address);
-  }
-}
-const student1 = new Student("Villain", 20, "Gotham");
+// class Parent {
+//   name: string;
+//   age: number;
+//   address: string;
+//   constructor(name: string, age: number, address: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//   }
+//   makeSleep(hours: number): string {
+//     return `This ${name} will sleep for ${hours} `;
+//   }
+// }
+// class Student extends Parent {
+//   constructor(name: string, age: number, address: string) {
+//     super(name, age, address);
+//   }
+// }
+// const student1 = new Student("Villain", 20, "Gotham");
 
-class Teacher extends Parent {
-  designation: string;
-  constructor(name: string, age: number, address: string, designation: string) {
-    super(name, age, address);
-    this.designation = designation;
-  }
-  takeClass(numOfClass: number): string {
-    return `This ${this.name} will take ${numOfClass} class`;
-  }
-}
-const teacher1 = new Teacher("Joker", 54, "Gotham", "Villain");
+// class Teacher extends Parent {
+//   designation: string;
+//   constructor(name: string, age: number, address: string, designation: string) {
+//     super(name, age, address);
+//     this.designation = designation;
+//   }
+//   takeClass(numOfClass: number): string {
+//     return `This ${this.name} will take ${numOfClass} class`;
+//   }
+// }
+// const teacher1 = new Teacher("Joker", 54, "Gotham", "Villain");
